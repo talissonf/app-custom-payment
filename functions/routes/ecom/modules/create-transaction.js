cc088928ebd324a83e8300c0f42e503119177074
Talisson Ferreira <talissonf@gmail.com>
2020-04-05 13:28:29 -0300

    feat(create-transaction): handle transaction

--- functions/routes/ecom/modules/create-transaction.js
+++ functions/routes/ecom/modules/create-transaction.js
@@ -1,3 +1,39 @@
 exports.post = ({ appSdk }, req, res) => {
-  res.send({})
+  appSdk
+    .getAuth(req.storeId)
+    .then(() => {
+      const { params } = req.body
+      const { amount } = params
+      const transaction = {
+        intermediator: {
+          payment_method: params.payment_method
+        },
+        currency_id: params.currency_id,
+        currency_symbol: params.currency_symbol,
+        discount: amount.discount,
+        amount: amount.total,
+        status: {
+          current: 'pending'
+        },
+        flags: [
+          'app-custom-payments'
+        ]
+      }
+
+      const payload = {
+        redirect_to_payment: false,
+        transaction
+      }
+
+      res.send(payload)
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(err.statusCode || 500)
+      const { message } = err
+      res.send({
+        error: 'CREATE_TRANSACTION_ERR',
+        message
+      })
+    })
 }
